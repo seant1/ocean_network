ce1117193e3b59a5b715e4a5924c492bc5fc5196
seant1 <sean.tran@outlook.com.au>
2020-02-25 15:16:52 +1100

    Open/close message

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,48 +8,119 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   String activeAnimation = 'idle';
+  bool messageOpen = false;
+  bool messageEditing = false;
+  bool messageOpenable = false;
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => print('TAP'),
-      onVerticalDragEnd: (details) {
-        if (details.primaryVelocity < 0) {
-          print('SWIPE UP');
-        } else if (details.primaryVelocity > 0) {
-          setState(() => activeAnimation = 'start');
-          print('SWIPE DOWN');
-        } else {
-          print('DRAG ZERO');
-        }
-      },
-      child: Stack(
-        children: <Widget>[
-          Container(
-            color: Colors.grey[100],
-          ),
-          FlareActor(
-            'assets/waves.flr',
-            alignment: Alignment.center,
-            fit: BoxFit.fitWidth,
-            animation: activeAnimation,
-            callback: (callback) {
-              setState(() => activeAnimation = 'idle');
-              print('Animation completed: waves.flr');
-            },
-          ),
-          FlareActor(
-            'assets/bottle-in.flr',
-            alignment: Alignment.center,
-            fit: BoxFit.fitWidth,
-            animation: activeAnimation,
-            callback: (callback) {
-              setState(() => activeAnimation = 'idle');
-              print('Animation completed: bottle-in.flr');
-            },
-          ),
-        ],
+    return Stack(children: <Widget>[
+      GestureDetector(
+        // onTap: () => print('TAP'),
+        onVerticalDragEnd: (details) {
+          if (details.primaryVelocity < 0) {
+            if (messageOpen) setState(() => messageOpen = false);
+            print('SWIPE UP');
+          } else if (details.primaryVelocity > 0) {
+            setState(() =>
+                messageOpen ? messageOpen = false : activeAnimation = 'start');
+            print('SWIPE DOWN');
+          } else {
+            if (messageOpen) {
+              setState(() {
+                messageOpen = false;
+                messageOpenable = true;
+              });
+            }
+            print('DRAG ZERO');
+          }
+        },
+        child: Stack(
+          children: <Widget>[
+            Container(
+              color: Colors.grey[100],
+            ),
+            FlareActor(
+              'assets/waves.flr',
+              alignment: Alignment.center,
+              fit: BoxFit.fitWidth,
+              animation: activeAnimation,
+              callback: (callback) {
+                setState(() => activeAnimation = 'idle');
+                print('Animation completed: waves.flr');
+              },
+            ),
+            FlareActor(
+              'assets/bottle-in.flr',
+              alignment: Alignment.center,
+              fit: BoxFit.fitWidth,
+              animation: activeAnimation,
+              callback: (callback) {
+                setState(() {
+                  activeAnimation = 'idle';
+                  messageOpenable = true;
+                });
+                print('Animation completed: bottle-in.flr');
+              },
+            ),
+            AnimatedOpacity(
+              // visible: messageOpen ? true : false,
+              opacity: messageOpen ? 1 : 0,
+              duration: Duration(milliseconds: 200),
+              child: Center(
+                child: Container(
+                  margin:
+                      EdgeInsets.symmetric(horizontal: 30.0, vertical: 90.0),
+                  padding: EdgeInsets.all(35.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30.0),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                          blurRadius: 60.0,
+                          offset: Offset(5, 10),
+                          spreadRadius: -25),
+                    ],
+                  ),
+                  child: Text(
+                    '"You’ll stop worrying what others think about you when you realize how seldom they do" - David Foster Wallace',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.normal,
+                      fontSize: 25,
+                      decoration: TextDecoration.none,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
-    );
+      Visibility(
+        visible: messageOpenable ? true : false,
+        child: Center(
+          // bottle tap detector
+          heightFactor: 6.5,
+          child: Container(
+            color: Colors.transparent,
+            width: 100,
+            height: 100,
+            alignment: Alignment.center,
+            child: GestureDetector(
+              onTap: () {
+                print('TAP: bottle');
+                setState(() {
+                  messageOpen = true;
+                  messageOpenable = false;
+                });
+              },
+            ),
+          ),
+        ),
+      ),
+    ]);
   }
 }
