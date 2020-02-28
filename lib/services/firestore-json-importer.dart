bfa5969e3f96748138507cac2e23a48be2da151d
seant1 <sean.tran@outlook.com.au>
2020-02-28 12:32:02 +1100

    Create Firestore JSON importer

---
+++ lib/services/firestore-json-importer.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart';
+
+/// running FirestoreImporter().importMessages() will import JSON data from assets
+
+class FirestoreImporter {
+  
+  String jsonFile = 'assets/test.json'; // works with array of JSON objects
+  String jsonImportField = 'text'; // String field of each JSON object
+  
+  final CollectionReference messageCollection =
+      Firestore.instance.collection('messages');
+
+  void importMessages() async {
+    String jsonString = await rootBundle.loadString(jsonFile);
+    final List<dynamic> jsonData = jsonDecode(jsonString);
+    jsonData.forEach((message) async {
+      var messageMap = _parseMessageBody(message[jsonImportField]);
+      // await messageCollection.document().setData(messageMap);
+      print('db-POST: $messageMap');
+    });
+  }
+
+  Map<String, dynamic> _parseMessageBody(String messageBody) {
+    return {
+      'body': messageBody,
+      'uid': 'import',
+      'timestamp': Timestamp.now(),
+      'score': 3,
+    };
+  }
+}
